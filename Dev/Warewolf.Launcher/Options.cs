de226501c47671a089e67f9c8b5cce60a2214ef1
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-08-28 09:37:45 +0200

    Revert "WOLF-4022 improve parallelize switch feedback"
    This reverts commit f95ad4705ef4c98a0230e2e8b21a39d9f29f9c94.

--- Dev/Warewolf.Launcher/Options.cs
+++ Dev/Warewolf.Launcher/Options.cs
@@ -307,7 +307,6 @@ namespace Warewolf.Launcher
                 }
                 if (options.Parallelize)
                 {
-                    Console.WriteLine("Parallelize: Running tests in parallel within each job.");
                     testLauncher.Parallelize = true;
                 }
                 if (options.StartContainer != null)
