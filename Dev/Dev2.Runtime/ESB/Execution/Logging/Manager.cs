c52fef9afd3a29ca103d472a5939228f3ad18cea
Pieter.Terblanche <pete.terblanche@gmail.com>
2018-10-11 15:41:25 +0200

    4138: Add Manager class

---
+++ Dev/Dev2.Runtime/ESB/Execution/Logging/Manager.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Dev2.Runtime.ESB.Execution.Logging
+{
+    public class Manager
+    {
+        // live.db
+
+        // main.db
+
+        // main.datetime.db
+
+        // ...
+    }
+}
